90c9e6f3b356d41299625d78dba20dad399e855c
TasosFrago <tasosfragkopoulos03@gmail.com>
2021-04-29 05:14:10 +0300

    new version

--- organizer_for_Linux.py
+++ organizer_for_Linux.py
@@ -1,35 +1,65 @@
 import shutil
 import glob
 import os
+import subprocess
 
-document_dir = '/home/pi/Documents/'
-audio_dir = '/home/pi/Music/'
-video_dir = '/home/pi/Videos/'
-picture_dir = '/home/pi/Pictures/'
-pythonscripts_dir = '/home/pi/Python/'
+default_dir = ["DESKTOP", "DOWNLOAD", "TEMPLATES", "PUBLICSHARE", "DOCUMENTS", "MUSIC", "PICTURES", "VIDEOS"]
 
-document_extensions = ['.doc', '.docx', '.log', '.msg', '.odt', '.pages', '.rtf', '.tex', '.txt', '.wpd', '.wps']
-audio_extensions = ['.aif', '.aac', '.iff', '.m3u', '.m4a', '.mid', '.mp3', '.mpa', '.wav', '.wma']
-video_extensions = ['.3g2', '.3gp', '.asf', '.avi', '.flv', '.m4v', '.mov', '.mp4', '.mpg', '.rm', '.srt', '.swf', '.vob', '.wmv']
-picture_extensions = ['.3dm', '.3ds', '.max', '.obj', '.bmp', '.dds', '.gif', '.heic', '.jpg', '.png', '.psp', '.pspimage', '.tga', '.thm', '.tif', '.tiff', '.yuv', '.ai', '.eps', '.svg']
-python_extensions = ['.egg-info', '.epp', '.oog', '.p', '.p4a', '.pickle', '.pil', '.pth', '.py', '.pyc', '.pyd', '.pyo', '.pyt', '.pyw', '.pyz', '.pyzw', '.rpy', '.ssdf', '.whl', '.yaml']
+class Organizer:
 
-for file in glob.glob(f'/home/pi/Downloads/*{document_extensions}'):
-    shutil.move(file, document_dir)
-    print(f'moved {file} to {document_dir}')
+    def __init__(self, directories=None):
+        # Set self.directories only if a list is passed
+        if isinstance(directories, list):
+            self.directories = []
+            for i in directories:
+                if os.path.exists(i) and os.path.isdir(i):
+                    self.directories.append(i)
+        # Check if a list is passed and make an empty list if not
+        elif isinstance(directories, type(None)):
+            self.directories = []
+        else:
+            raise Exception("Error: `directories` must be a list")
 
-for file in glob.glob(f'/home/pi/Downloads/*{audio_extensions}'):
-    shutil.move(file, audio_dir)
-    print(f'moved {file} to {audio_dir}')
+        # Execute system command to get the paths for the default directories 
+        for i in default_dir:
+            output = subprocess.check_output(['xdg-user-dir', i])
+            output = output.decode("utf-8").rstrip()
+            self.directories.append(output)
 
-for file in glob.glob(f'/home/pi/Downloads/*{video_extensions}'):
-    shutil.move(file, video_dir)
-    print(f'moved {file} to {video_dir}')
+        print(self.directories)
 
-for file in glob.glob(f'/home/pi/Downloads/*{picture_extensions}'):
-    shutil.move(file, picture_dir)
-    print(f'moved {file} to {picture_dir}')
+    def get_directory(self):
+        pass
 
-for file in glob.glob(f'/home/pi/Downloads/*{python_extensions}'):
-    shutil.move(file, pythonscripts_dir)
-    print(f'moved {file} to {pythonscripts_dir}')
+types = [
+    {
+        'directory': '/home/pi/Documents/',
+        'extensions': ['.doc', '.docx', '.log', '.msg', '.odt', '.pages', '.rtf', '.tex', '.txt', '.wpd', '.wps'],
+    },
+    {
+        'directory': '/home/pi/Music/',
+        'extensions': ['.aif', '.aac', '.iff', '.m3u', '.m4a', '.mid', '.mp3', '.mpa', '.wav', '.wma'],
+    },
+    {'directory': '/home/pi/Videos/',
+        'extensions': ['.3g2', '.3gp', '.asf', '.avi', '.flv', '.m4v', '.mov', '.mp4', '.mpg', '.rm', '.srt', '.swf', '.vob', '.wmv'],
+    },
+    {
+        'directory': '/home/pi/Pictures/',
+        'extensions': ['.3dm', '.3ds', '.max', '.obj', '.bmp', '.dds', '.gif', '.heic', '.jpg', '.png', '.psp', '.pspimage', '.tga', '.thm', '.tif', '.tiff', '.yuv', '.ai', '.eps', '.svg'],
+    },
+    {
+        'directory': '/home/pi/Python/',
+        'extensions': ['.egg-info', '.epp', '.oog', '.p', '.p4a', '.pickle', '.pil', '.pth', '.py', '.pyc', '.pyd', '.pyo', '.pyt', '.pyw', '.pyz', '.pyzw', '.rpy', '.ssdf', '.whl', '.yaml'],
+    },
+]
+
+def move():
+    for i in types:
+        for file in glob.glob(f"/home/pi/Downloads/*{i['extensions']}"):
+            shutil.move(file, i['directory'])
+            # print(f'moved {file} to {document_dir}')
+
+
+if __name__=='__main__':
+    li = ["/home/tasos/projects/test/directory-organizer/test", "/home/tasos/downloads"]
+    test = Organizer(li)
