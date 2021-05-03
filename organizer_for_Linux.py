f80ab368ba38255df101328ce7851134a9f5f1a2
TasosFrago <tasosfragkopoulos03@gmail.com>
2021-05-03 16:04:38 +0300

    Added the ability to create configuration file

--- organizer_for_Linux.py
+++ organizer_for_Linux.py
@@ -3,6 +3,9 @@ import glob
 import os
 import subprocess
 
+homedir = os.path.expanduser("~")
+configdir = os.path.join(homedir, ".config")
+
 # 'extensions': ['.egg-info', '.epp', '.oog', '.p', '.p4a', '.pickle', '.pil', '.pth', '.py', '.pyc', '.pyd', '.pyo', '.pyt', '.pyw', '.pyz', '.pyzw', '.rpy', '.ssdf', '.whl', '.yaml'],
 
 default_dir = ["DOWNLOAD", "DOCUMENTS", "MUSIC", "PICTURES", "VIDEOS"]
@@ -36,33 +39,34 @@ class Organizer:
         # Find the xdg path for the default directories
         self.default_types = default_types
         for i in self.default_types:
-            dir_path = subprocess.check_output(['xdg-user-dir', i['name']]) # Use system call to get the xdg dir path
-            dir_path = dir_path.decode("utf-8").rstrip()
+            dir_path = subprocess.check_output(['xdg-user-dir', i['name']]).decode("utf-8").rstrip() # Use system call to get the xdg dir path
             i["directory"] = dir_path
 
     def get_directory(self):
         pass
 
     def initialize_config(self):
-        with open("config.py", "w+") as config:
-            config.write("""
-extra_paths = [
-    {
-        'name': "",
-        'directory': "",
-        'extensions': "",
-    }
-]
-""")
+        config_path = os.path.join(configdir, "directory_organizer")
+        config_file = os.path.join(config_path, "config.py")
+        if os.path.exists(os.path.join(config_path)):
+            if os.path.isfile(config_file):
+                print("Configuration file already exists")
+            else:
+                shutil.copyfile("config.py", config_file)
+        else:
+            os.mkdir(config_path)
+            shutil.copyfile("config.py", config_file)
 
 
-def move():
-    for i in types:
-        for file in glob.glob(f"/home/pi/Downloads/*{i['extensions']}"):
-            shutil.move(file, i['directory'])
-            # print(f'moved {file} to {document_dir}')
+# def move():
+#     for i in types:
+#         for file in glob.glob(f"/home/pi/Downloads/*{i['extensions']}"):
+#             shutil.move(file, i['directory'])
+#             # print(f'moved {file} to {document_dir}')
 
 
 if __name__=='__main__':
     li = ["/home/tasos/projects/test/directory-organizer/test", "/home/tasos/downloads"]
     test = Organizer(li)
+    test.initialize_config()
+    print(os.path.realpath("./config.py"))
